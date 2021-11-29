b16dc885d4dbfb5f1a24d0421d4f24ab5f073152
Stjushenka <stjusheka@gmail.com>
2021-11-29 18:35:42 +0300

    Add main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 12000;
+        int bonus = 20;
+        int mile;
+        mile = cost/bonus;
+        System.out.println("Твои мили за покупку: " + mile);
+    }
+}
